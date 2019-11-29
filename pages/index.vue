fbc99348179f9ba5dc057cbe0d20a1f85ee6d733
nezlicodes <benmeradi.lilyanezli@gmail.com>
2019-11-29 18:31:44 +0100

    fills home page with content and routin

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="container">
-    
+    <div class="py-4 d-flex justfy-content-center flex-column">
+  <b-card img-src="https://placekitten.com/300/300" img-alt="Card image" img-left class="mb-3">
+      <nuxt-link to="/posts/1">
+      <b-card-text>
+        Some quick example text to build on the card and make up the bulk of the card's content.
+      </b-card-text>
+      </nuxt-link>
+    </b-card>
+
+
+    <b-card img-src="https://placekitten.com/300/300" img-alt="Card image" img-right>
+      <nuxt-link to="/posts/1">
+      <b-card-text>
+        Some quick example text to build on the card and make up the bulk of the card's content.
+      </b-card-text>
+      </nuxt-link>
+    </b-card>
+    </div>
   </div>
 </template>
 
@@ -19,6 +36,7 @@ export default {
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   text-align: center;
